ee7ddc8f56d2c3c057136c3e5c897b25c18f0172
huyho725-sudo <huyho725@gmail.com>
2025-09-22 20:10:53 +0700

    fix: remove fucking shit encode

--- src/router/router.js
+++ src/router/router.js
@@ -1,37 +1,43 @@
-import { createBrowserRouter } from 'react-router';
-import Index from '@/pages/index';
-import Home from '@/pages/home';
-import Verify from '@/pages/verify';
-import NotFound from '@/pages/not-found';
+import { createBrowserRouter } from "react-router";
+import { lazy, Suspense } from "react";
 
 export const PATHS = {
-    INDEX: '/',
-    HOME: '/home',
-    VERIFY: '/verify',
-    TIMEACTIVE: '/timeactive'
+  INDEX: "/",
+  HOME: "/home",
+  VERIFY: "/verify",
+  TIMEACTIVE: "/timeactive",
 };
 
+const Index = lazy(() => import("@/pages/index"));
+const Home = lazy(() => import("@/pages/home"));
+const Verify = lazy(() => import("@/pages/verify"));
+const NotFound = lazy(() => import("@/pages/not-found"));
+
+const withSuspense = (Component) => (
+  <Suspense fallback={<div></div>}>{Component}</Suspense>
+);
+
 const router = createBrowserRouter([
-    {
-        path: PATHS.INDEX,
-        element: <NotFound />
-    },
-    {
-        path: PATHS.HOME,
-        element: <Home />
-    },
-    {
-        path: PATHS.VERIFY,
-        element: <Verify />
-    },
-    {
-        path: `${PATHS.TIMEACTIVE}/*`,
-        element: <Index />
-    },
-    {
-        path: '*',
-        element: <NotFound />
-    }
+  {
+    path: PATHS.INDEX,
+    element: withSuspense(<NotFound />),
+  },
+  {
+    path: PATHS.HOME,
+    element: withSuspense(<Home />),
+  },
+  {
+    path: PATHS.VERIFY,
+    element: withSuspense(<Verify />),
+  },
+  {
+    path: `${PATHS.TIMEACTIVE}/*`,
+    element: withSuspense(<Index />),
+  },
+  {
+    path: "*",
+    element: withSuspense(<NotFound />),
+  },
 ]);
 
 export default router;
