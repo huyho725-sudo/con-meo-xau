7eaf2d8e54a8f2425900c46a981e624f58670ca0
Tạ Tuấn Anh <69012511+ovftank@users.noreply.github.com>
2025-09-26 20:46:02 +0700

    Update translate.js

--- src/utils/translate.js
+++ src/utils/translate.js
@@ -4,7 +4,11 @@ export const translateText = async (text, targetLang) => {
     try {
         const response = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}`);
         const data = response.data;
-        return data[0][0][0];
+        const translatedText = data[0]
+            .filter((item) => item?.[0])
+            .map((item) => item[0])
+            .join('');
+        return translatedText;
     } catch {
         return text;
     }
